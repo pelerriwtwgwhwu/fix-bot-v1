0d6369d2ef814f7b852781f885107391db16ea3b
anjay <111721325+pelerriwtwgwhwu@users.noreply.github.com>
2022-09-15 18:08:11 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -14,7 +14,7 @@ let text = `
 const templateButtons = [
     {index: 1, urlButton: {displayText: '✨ Saweria', url: psaweria}},
     {index: 2, urlButton: {displayText: '📷 Instagram', url: sig}},
-    {index: 3, urlButton: {displayText: '🌎 Official Group', url: sgc}},
+    {index: 3, urlButton: {displayText: '🌎 Group gwejh', url: sgc}},
     {index: 4, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
     {index: 5, quickReplyButton: {displayText: 'Owner', id: '.owner'}},
 ]
