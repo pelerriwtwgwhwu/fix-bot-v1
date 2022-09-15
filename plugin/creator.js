e1f17ab9d88d2acd660ea5ba7da42ad11c685f5b
anjay <111721325+pelerriwtwgwhwu@users.noreply.github.com>
2022-09-15 18:00:51 +0700

    Update and rename plugins/creator.js to plugin/creator.js

--- plugin/creator.js
+++ plugin/creator.js
@@ -20,8 +20,8 @@ let handler  = async (m, { conn, command, args, usedPrefix, DevMode }) => {
 //------------ BIO
 let ppown = await conn.profilePictureUrl(nomorown + '@s.whatsapp.net', 'image').catch(_ => hwaifu[1]) 
 let teksbio = `${htki} *BIODATA* ${htka}
-${htjava} *💌 Nama* : WH-MODS-Dev
-${htjava} *✉️ Nama RL* : HAMBA ALLAH
+${ htjava }  *💌 Nama* : FARUQFARUQ 
+${  htjava  }   *✉️ Nama RL* : HAMBA ALLAH
 ${htjava} *♂️ Gender* : Boys
 ${htjava} *🕋 Agama* : Islam
 ${htjava} *⏰ Tanggal lahir* : Private 🥶
